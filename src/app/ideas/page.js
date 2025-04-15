e899537d33063d934768fd8671c8535a5a8171ef
ÖMER KISAKOL <okisakol@gmail.com>
2025-04-15 22:49:15 +0300

    fix: Adjust margin for user info display in IdeasPage component

--- src/app/ideas/page.js
+++ src/app/ideas/page.js
@@ -110,7 +110,7 @@ export default function IdeasPage() {
           </Link>
           
           {user && (
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 mr-2">
               <div className="text-white bg-indigo-700 px-4 py-2 rounded-lg flex items-center gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
